eac7d983c856a75a28c761537dac5b6a76accfd2
PraveenR1510 <rpraveenravi06@gmail.com>
2026-08-12 21:31:03 +0530

    Create 838-DesignLinkedList

---
+++ 838-DesignLinkedList/838-DesignLinkedList.java
@@ -0,0 +1,76 @@
+// Last updated: 8/12/2026, 9:31:08 PM
+class MyLinkedList {
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    private ListNode dummy;
+    private int size;
+
+    public MyLinkedList() {
+        dummy = new ListNode(0);
+        size = 0;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+
+        ListNode curr = dummy.next;
+        for (int i = 0; i < index; i++) {
+            curr = curr.next;
+        }
+
+        return curr.val;
+    }
+
+    public void addAtHead(int val) {
+        addAtIndex(0, val);
+    }
+
+    public void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index > size) {
+            return;
+        }
+
+        if (index < 0) {
+            index = 0;
+        }
+
+        ListNode prev = dummy;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+
+        ListNode newNode = new ListNode(val);
+        newNode.next = prev.next;
+        prev.next = newNode;
+
+        size++;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+
+        ListNode prev = dummy;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+
+        prev.next = prev.next.next;
+        size--;
+    }
+}
