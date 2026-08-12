dd3693c9f6064132960619727fe747edbd12c830
PraveenR1510 <rpraveenravi06@gmail.com>
2026-08-12 21:31:32 +0530

    Create 452-MinimumNumberOfArrowsToBurstBalloons

---
+++ 452-MinimumNumberOfArrowsToBurstBalloons/452-MinimumNumberOfArrowsToBurstBalloons.java
@@ -0,0 +1,17 @@
+// Last updated: 8/12/2026, 9:31:37 PM
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points,(a,b)-> Integer.compare(a[1],b[1]));
+        int n=0;
+        long end=Long.MIN_VALUE;
+        for(int[] baloon : points ){
+            if(baloon[0]<=end)
+            continue;
+            else{
+                n++;
+                end=baloon[1];
+            }
+       
+    }
+    return n;
+    }}
